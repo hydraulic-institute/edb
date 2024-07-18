2966dc376efb62698fb789d5354cdf8736807b98
Kristin Knight <kristin@kdahl.com>
2024-07-17 22:26:07 -0500

    EDL-46 continued

--- source/javascript/base.js
+++ source/javascript/base.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function() {
-    setup_menu();
     $('#unit-menu').show();
     $('#mobile-menu').show();
 });
 
 $(window).on('load',function() {
+    setup_menu();
     $('#fullpage').show();
 });
 
@@ -26,40 +26,56 @@ MathJax.Hub.Queue(
     }
 );
 
+function pathToId(path) {
+    let parts = path.split("/");
+    parts.splice(0,1);
+    let id = parts.join("_");
+    id = id.replace(".html","");
+    return id;
+}
+
 function setup_menu() {      
     //Update the menu to show the active topic and any uncollapsed parents
     //Get the active window
     //Save the active window
     //What if search sets active window?
+    if ($('.navbar-burger').is(':visible')) {
+        return;
+    }
     let active_topic = $(".active_topic");
     let default_topic = $(".default_topic");
     let current_topic = active_topic;
     if (!active_topic.length) {
         current_topic = default_topic;
-    }   
+    }  
+    let current_topic_id = current_topic.attr('id');
+    let current_topic_href = current_topic.attr('href'); 
+    let current_path = window.location.pathname;
+    //If the current path is not the same as the current topic, update the current topic
+    if ((current_path != current_topic_href) && (current_path != "/")) {
+        current_topic_id = pathToId(current_path);
+        current_topic_href = current_path;
+    }
     let storage_active_topic = localStorage.getItem("active_topic");
     if (!storage_active_topic) {
-        storage_active_topic = {'topic':'', 'href':''};
-        storage_active_topic['topic'] = current_topic.attr('id');
-        storage_active_topic['href'] = current_topic.attr('href');
+        storage_active_topic = {"topic":current_topic_id, "href":current_topic_href};
         localStorage.setItem("active_topic", JSON.stringify(storage_active_topic));
     }
     else {
         storage_active_topic = JSON.parse(storage_active_topic);
-        // if (current_topic.attr('id') != storage_active_topic['topic']) {
-        //     storage_active_topic['topic'] = current_topic.attr('id');
-        //     storage_active_topic['href'] = current_topic.attr('href');
-        //     localStorage.setItem("active_topic", JSON.stringify(storage_active_topic));
-        //     if (current_topic != default_topic) {
-        //         window.location.href = storage_active_topic['href'];
-        //         return;
-        //     }
-        // }
-        if (storage_active_topic['href'] != window.location.pathname) {
+    }
+    if (storage_active_topic['href'] != current_topic_href) {
+        if (window.location.pathname == "/") {
             window.location.href = storage_active_topic['href'];
             return;
         }
+        else {
+            storage_active_topic['href'] = current_topic_href;
+            storage_active_topic['topic'] = current_topic_id;
+            localStorage.setItem("active_topic", JSON.stringify(storage_active_topic));
+        }
     }
+    //Set the active topic
     $("#"+storage_active_topic['topic']).addClass("active_topic");
     $("#"+storage_active_topic['topic']).addClass("is-active");
 
@@ -115,8 +131,28 @@ function menu_topic_click(event) {
     //Save open menu items to local storage
     event.stopPropagation();
     event.stopImmediatePropagation();
-    var target = $(this).attr('id');
-    console.log('topic:', target);
-    let active_topic = {"topic":target, "href":$(this).attr('href')};
-    localStorage.setItem("active_topic", JSON.stringify(active_topic));
+    $('.menu-topic').removeClass("active_topic");
+    $('.menu-topic').removeClass("is-active");
+    let target = $(this).attr('id');
+    let href = $(this).attr('href');
+    if (!target) {
+        target = pathToId(window.location.pathname);
+        href = window.location.pathname;
+        let expanded_sections = localStorage.nav_show;
+        expanded_sections = JSON.parse(expanded_sections);
+        //Expand the new section 
+        let section = "#"+target.split("_")[0];
+        if (expanded_sections.indexOf(section) == -1) {
+            expanded_sections.push(section);
+            localStorage.setItem('nav_show', JSON.stringify(expanded_sections));
+        }
+        $('.menu-section').unbind('click');
+        for (let section of expanded_sections) {
+            $(section+"-button").click();
+        }
+        $('.menu-section').bind('click', menu_click);
+        $("#"+target).addClass("active_topic");
+    }
+    $("#"+target).addClass("is-active");
+    localStorage.setItem("active_topic", JSON.stringify({"topic":target, "href":href}));
 }
